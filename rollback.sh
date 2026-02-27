cba5499fcca2425ada14ed562b18b630c152af62
Xqhare <xqhare@web.de>
2026-02-27 12:54:05 +0100

    add: rollback.sh

---
+++ rollback.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# rollback.sh for blog service (Stub)
+# This service does not yet have a data directory structure.
+
+echo "Rollback for blog service is not yet implemented."
+exit 0
