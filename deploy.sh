3ccbdb9cdbfc095f3721c73d65e16555e359351b
Xqhare <xqhare@web.de>
2026-03-01 00:04:42 +0100

    revert: remove sudo from deploy and rollback scripts

--- deploy.sh
+++ deploy.sh
@@ -10,18 +10,18 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$THIS_DIR"
 
 if ! test -d "$THIS_DIR/html"; then
-        sudo mkdir -p "$THIS_DIR/html"
+        mkdir -p "$THIS_DIR/html"
 fi
 
 # Deploy assumes that any building has already been done
 BUILD_DIR="$THIS_DIR/build"
 DATA_DIR="$THIS_DIR/data"
 if ! test -d "$DATA_DIR"; then
-        sudo mkdir -p "$DATA_DIR"
+        mkdir -p "$DATA_DIR"
 fi
 
 DEPLOYMENT_DIR="$DATA_DIR/$(date +%Y-%m-%d-%H-%M-%S)"
-sudo mkdir -p "$DEPLOYMENT_DIR"
+mkdir -p "$DEPLOYMENT_DIR"
 echo "Setting up environment done."
 echo "- - - - - - - - - - - - - - - - - - - - - - - -"
 echo #
@@ -29,29 +29,29 @@ echo #
 echo "Deploying blog pages..."
 # 1. move build data to data directory
 echo "Moving build data to data directory..."
-sudo mv "$BUILD_DIR/landing.html" "$DEPLOYMENT_DIR/index.html"
-sudo mv "$BUILD_DIR/posts" "$DEPLOYMENT_DIR/posts"
-sudo mv "$BUILD_DIR/ContentsTable" "$DEPLOYMENT_DIR/ContentsTable"
+mv "$BUILD_DIR/landing.html" "$DEPLOYMENT_DIR/index.html"
+mv "$BUILD_DIR/posts" "$DEPLOYMENT_DIR/posts"
+mv "$BUILD_DIR/ContentsTable" "$DEPLOYMENT_DIR/ContentsTable"
 echo "Moving done."
 echo #
 
 echo "Updating favicon and logo..."
-sudo cp "$BUILD_DIR/favicon.png" "$THIS_DIR/html/favicon.png"
-sudo cp "$BUILD_DIR/logo.png" "$THIS_DIR/html/logo.png"
+cp "$BUILD_DIR/favicon.png" "$THIS_DIR/html/favicon.png"
+cp "$BUILD_DIR/logo.png" "$THIS_DIR/html/logo.png"
 echo "Updating favicon and logo done."
 echo #
 
 echo "Creating symlinks..."
 cd "$THIS_DIR/html"
-sudo ln -s -f "../data/$(basename "$DEPLOYMENT_DIR")/index.html" "index.html"
+ln -s -f "../data/$(basename "$DEPLOYMENT_DIR")/index.html" "index.html"
 
 # Symlink the posts directory
-sudo rm -f "posts"
-sudo ln -s -f "../data/$(basename "$DEPLOYMENT_DIR")/posts" "posts"
+rm -f "posts"
+ln -s -f "../data/$(basename "$DEPLOYMENT_DIR")/posts" "posts"
 
 # Symlink the ContentsTable directory
-sudo rm -f "ContentsTable"
-sudo ln -s -f "../data/$(basename "$DEPLOYMENT_DIR")/ContentsTable" "ContentsTable"
+rm -f "ContentsTable"
+ln -s -f "../data/$(basename "$DEPLOYMENT_DIR")/ContentsTable" "ContentsTable"
 echo "Symlinks created."
 echo #
 
